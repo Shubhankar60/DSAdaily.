8ec268374680763a76c5bacc5acf83821d337864
shubhankar <156128209+Shubhankar60@users.noreply.github.com>
2025-01-09 08:15:22 +0530

    Add files via upload

---
+++ reversenum.java
@@ -0,0 +1,18 @@
+public class reversenum {
+    public static int revnum(int n){
+       int ans =0;
+       int lastdig=0;
+        while(n>0){
+          lastdig=n%10;
+          n=n/10;
+           ans=ans*10+lastdig;
+       
+        }
+       return ans;
+    }
+public static void main(String args[]){
+    int n=123456;
+   int finalans=revnum(n);
+   System.out.println(finalans);
+}
+}
